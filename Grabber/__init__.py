f057b6ed41d05a09bc7ae0442f3df7b95f0a2167
Tosu <167279662+about-tosu@users.noreply.github.com>
2025-02-06 02:31:22 +0530

    Update __init__.py

--- Grabber/__init__.py
+++ Grabber/__init__.py
@@ -16,15 +16,15 @@ logging.getLogger('httpx').setLevel(logging.WARNING)
 logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
-OWNER_ID = ["6965147961","6848223695"]
-SUDO_USERS = ["6848223695","6965147961","5897492421","6745981296","1374057577","5458968679","6309921371"]
+OWNER_ID = ["6848223695"]
+SUDO_USERS = ["6848223695"]
 GROUP_ID = -1002332668271
-TOKEN = "7749407611:AAEBsTIO6hRVNiD_tApKeOJVBYJAffGnMtc" 
+TOKEN = "7215759920:AAH8pwxz7bHge8Buv71FlJMx10ErsjrD5Io" 
 mongo_url = "mongodb+srv://snatcherwaifu0925:snatcherwaifu0925@snatcher0.fft3j.mongodb.net/?retryWrites=true&w=majority&appName=Snatcher0"
 PHOTO_URL = ["https://envs.sh/nJo.jpg", "https://envs.sh/nJs.jpg"]
 SUPPORT_CHAT = "-1002332668271"
 UPDATE_CHAT = "-1002332668271"
-BOT_USERNAME = "SNATCH_YOURR_WAIFU_BOT"
+BOT_USERNAME = "GuessXGameBot"
 CHARA_CHANNEL_ID = "-1002336624454"
 api_id = "24835491"
 api_hash = "04ee66f0079a9b11eefb33a89289899e" 
